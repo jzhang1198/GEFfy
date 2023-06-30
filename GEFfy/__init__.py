8b0b296bd90ea2d25330d5fe1566b9c88d75d09b
jzhang1198 <jzhang1198@gmail.com>
2023-06-30 10:42:52 -0700

    Update to enable installation to conda environment

--- GEFfy/__init__.py
+++ GEFfy/__init__.py
@@ -1,3 +1,9 @@
+"""
+GEFfy: a python library for fitting initial rates and Michaelis-Menten constants
+to GEF-catalyzed exchange, GAP-catalyzed hydrolysis, and potentially other 
+enzymatic reactions.
+"""
+
 from . import gef_fitter
 
 __version__ = "0.1.1"
